0269c6ea0bbd6f0c409a93a34294ce5fec2a59f6
keishi <ky0324880@gmail.com>
2020-06-24 10:15:02 +0900

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(1000);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+  const reset = () => {
+    setCount(1000);
+  };
+  const mul = () => {
+    setCount((previousCount) => previousCount * 2);
+  };
+  const divideThree = () => {
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+  };
   return (
-    <div>
-      <h2>Hello World</h2>
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={mul}>x2</button>
+        <button onClick={divideThree}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
   );
 };
 
